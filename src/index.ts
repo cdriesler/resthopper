1201e3f28c81751bc21c2f41d00303fdbdc52a3c
Chuck Driesler <cdriesler.iv@gmail.com>
2019-11-10 16:46:32 -0500

    add models to default export

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 import ComponentIndex from './catalog/ComponentIndex';
 import ParameterIndex from './catalog/ParameterIndex';
 import Definition from './models/ResthopperDefinition';
+import Component from './models/ResthopperComponent';
+import Parameter from './models/ResthopperParameter';
 import Parse from './predicates/Parse';
 
 export default {
     ComponentIndex,
     ParameterIndex,
     Definition,
+    Component,
+    Parameter,
     Parse
 }
 
