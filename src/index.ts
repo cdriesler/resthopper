7eb3a88407cb42793c0356868b2305d1c3f615e0
chuck driesler <cdriesler.iv@gmail.com>
2019-12-22 16:02:02 -0500

    revert commonjs

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
-// import ComponentIndex from './catalog/ComponentIndex';
-// import ParameterIndex from './catalog/ParameterIndex';
-// import Definition from './models/ResthopperDefinition';
-// import Component from './models/ResthopperComponent';
-// import Parameter from './models/ResthopperParameter';
-// import Parse from './predicates/Parse';
+import ComponentIndex from './catalog/ComponentIndex';
+import ParameterIndex from './catalog/ParameterIndex';
+import Definition from './models/ResthopperDefinition';
+import Component from './models/ResthopperComponent';
+import Parameter from './models/ResthopperParameter';
+import Parse from './predicates/Parse';
 
-// export default {
-//     ComponentIndex,
-//     ParameterIndex,
-//     Definition,
-//     Component,
-//     Parameter,
-//     Parse
-// }
+export default {
+    ComponentIndex,
+    ParameterIndex,
+    Definition,
+    Component,
+    Parameter,
+    Parse
+}
 
 
